bbfc89fe8dc4b2f9dbedd76643b625e3620a33d2
David Moravec <moravec@numsolution.cz>
2023-12-01 15:13:18 +0100

    Do not use references for copy types

--- src/bitboard.rs
+++ src/bitboard.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 use strum::IntoEnumIterator;
 
 use crate::piece::{Piece, Color};
+use crate::piece::Color::{BLACK, WHITE};
 use crate::square::Square;
 
 static ONE: u64 = 1;
@@ -9,6 +10,10 @@ static ZERO: u64 = 0;
 
 pub struct Board {
    bit_boards: HashMap<Color, HashMap<Piece, u64>>
+}
+
+fn square_occupied(bboard: u64, square: Square) -> bool {
+    bboard & (ONE << square as u64) == 0
 }
 
 impl Board {
@@ -24,8 +29,8 @@ impl Board {
         
         let mut board = Board { 
             bit_boards: HashMap::from([
-                (Color::WHITE, one_side_map.clone()),
-                (Color::BLACK, one_side_map), 
+                (WHITE, one_side_map.clone()),
+                (BLACK, one_side_map), 
             ])
         };
         
@@ -33,52 +38,68 @@ impl Board {
         print_board(&board);
         board
     }
+
     
-    fn pieces_bit_board(&self, piece: &Piece) -> u64 {
-        self.color_pieces_bit_board(&Color::BLACK, piece) 
+    fn pieces_bit_board(&self, piece: Piece) -> u64 {
+        self.color_pieces_bit_board(BLACK, piece) 
         | 
-        self.color_pieces_bit_board(&Color::WHITE, piece)
+        self.color_pieces_bit_board(WHITE, piece)
     }
 
-    fn pieces_bit_board_mut(&mut self, piece: &Piece) -> u64 {
-        self.color_pieces_bit_board_mut(&Color::BLACK, piece) 
+    fn pieces_bit_board_mut(&mut self, piece: Piece) -> u64 {
+        self.color_pieces_bit_board_mut(BLACK, piece) 
         | 
-        self.color_pieces_bit_board_mut(&Color::WHITE, piece)
+        self.color_pieces_bit_board_mut(WHITE, piece)
     }
     
-    fn color_pieces_bit_board(&self, color: &Color, piece: &Piece) -> u64 {
-        *self.bit_boards.get(color).unwrap()
-                        .get(piece).unwrap()
+    fn color_pieces_bit_board(&self, color: Color, piece: Piece) -> u64 {
+        *self.bit_boards.get(&color).unwrap()
+                        .get(&piece).unwrap()
     }
 
-    fn color_pieces_bit_board_mut(&mut self, color: &Color, piece: &Piece) -> u64 {
-        *self.bit_boards.get_mut(color).unwrap()
-                        .get_mut(piece).unwrap()
+    fn color_pieces_bit_board_mut(&mut self, color: Color, piece: Piece) -> u64 {
+        *self.bit_boards.get_mut(&color).unwrap()
+                        .get_mut(&piece).unwrap()
     }
     
-    fn set_color_pieces_bit_board(&mut self, color: &Color, piece: Piece, bitboard: u64) -> () {
-        self.bit_boards.get_mut(color).unwrap().insert(piece, bitboard);
+    fn set_color_pieces_bit_board(&mut self, color: Color, piece: Piece, bitboard: u64) -> () {
+        self.bit_boards.get_mut(&color).unwrap().insert(piece, bitboard);
     }
     
     fn fill_board(&mut self) -> () {
         fill_board_with_pawns(self);
     }
+
+    pub fn put_piece_on_square(
+        &mut self, color: Color,piece: Piece, square: Square
+    ) -> Result<(), &str> {
+        let mut bboard = self.color_pieces_bit_board_mut(color, piece);
+
+        if square_occupied(bboard, square) {
+            return Err("Square occupied")
+        } else {
+            bboard = bboard | (ONE << square as u64);
+            self.set_color_pieces_bit_board(color, piece, bboard);
+
+            return Ok(())
+        }
+    }
 }
 
 fn fill_board_with_pawns(board: &mut Board) -> () {
-    let mut black_pawn_bit_board = board.color_pieces_bit_board_mut(&Color::BLACK, &Piece::PAWN);
+    let mut black_pawn_bit_board = board.color_pieces_bit_board_mut(BLACK, Piece::PAWN);
     
     for i in Square::B1 as u64 ..= Square::B8 as u64 {
         black_pawn_bit_board = black_pawn_bit_board | (ONE << i);
     }
     print_board(board);
     
-    board.set_color_pieces_bit_board(&Color::BLACK, Piece::PAWN, black_pawn_bit_board);
+    board.set_color_pieces_bit_board(BLACK, Piece::PAWN, black_pawn_bit_board);
 }
 
 fn print_board(board: &Board) -> () {
     for piece in Piece::iter() {
-        let piece_bit_board = board.pieces_bit_board(&piece);
+        let piece_bit_board = board.pieces_bit_board(piece);
         
         for i in 0 .. 64 {
             if i % 8 == 0 && i > 0 {
