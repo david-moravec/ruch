7223f0ee35c703f6000ad73198d9365ccee7db40
David Moravec <moravec@numsolution.cz>
2023-12-19 13:51:36 +0100

    start working on pawns attacks

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 mod bitboard;
 mod piece;
 mod square;
+mod attack_maps;
 
 
 use crate::bitboard::{Board, fill_board_fen, print_board, DEFAULT_FEN};
+use crate::attack_maps::pawn_attack::test_setup;
 
 
 fn main() {
     let mut board = Board::new();
     fill_board_fen(&mut board, DEFAULT_FEN);
     print_board(&board);
+    let a = test_setup();
 }
