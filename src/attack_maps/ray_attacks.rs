cedf0516a97ccf33fb9e5d1c28228668b79b985a
David Moravec <moravec@numsolution.cz>
2024-01-22 15:25:23 +0100

    Add diagonal ray attacks

--- src/attack_maps/ray_attacks.rs
+++ src/attack_maps/ray_attacks.rs
@@ -1,6 +1,9 @@
 use crate::{
-    constants::{A1_H8_DIAG, A_FILE, EIGHT_RANK, H_FILE, ONE_RANK},
-    types::bitboard::BitBoard,
+    constants::{A1_H8_DIAG, A_FILE, EIGHT_RANK, H1_A8_DIAG, H_FILE, ONE_RANK},
+    types::bitboard::{
+        bitboard_to_str, east_one, north_east_one, north_west_one, south_east_one, south_west_one,
+        west_one, BitBoard,
+    },
 };
 use std::collections::HashMap;
 
@@ -40,6 +43,71 @@ const fn calculate_west_ray_attack(origin: BitBoard) -> BitBoard {
     source << origin.trailing_zeros() & not_rank_above
 }
 
+const fn calculate_north_west_ray_attack(origin: BitBoard) -> BitBoard {
+    let mut result: BitBoard = north_west_one(A1_H8_DIAG);
+
+    let trailing_zeros = origin.trailing_zeros();
+    let mut i = 0;
+
+    let file_count = trailing_zeros % 8;
+    let rank_count = trailing_zeros / 8;
+
+    while i < file_count {
+        result = west_one(result);
+        i += 1;
+    }
+
+    result << rank_count * 8
+}
+
+fn calculate_south_east_ray_attack(origin: BitBoard) -> BitBoard {
+    let mut result: BitBoard = south_east_one(A1_H8_DIAG);
+
+    let leading_zeros = origin.leading_zeros();
+    let mut i = 0;
+
+    let file_count = leading_zeros % 8;
+    let rank_count = leading_zeros / 8;
+
+    while i < file_count {
+        result = east_one(result);
+        i += 1;
+    }
+
+    result >> rank_count * 8
+}
+
+fn calculate_north_east_ray_attack(origin: BitBoard) -> BitBoard {
+    let mut result: BitBoard = north_east_one(H1_A8_DIAG);
+
+    let mut i = 0;
+
+    let file_count = origin.leading_zeros() % 8;
+    let rank_count = origin.trailing_zeros() / 8;
+
+    while i < file_count {
+        result = east_one(result);
+        i += 1;
+    }
+
+    result << rank_count * 8
+}
+
+fn calculate_south_west_ray_attack(origin: BitBoard) -> BitBoard {
+    let mut result: BitBoard = south_west_one(H1_A8_DIAG);
+
+    let mut i = 0;
+
+    let file_count = origin.trailing_zeros() % 8;
+    let rank_count = origin.leading_zeros() / 8;
+
+    while i < file_count {
+        result = west_one(result);
+        i += 1;
+    }
+
+    result >> rank_count * 8
+}
 fn calculate_ray_attacks_for_each_direction_pos(
     origin: BitBoard,
 ) -> HashMap<RayDirection, BitBoard> {
@@ -146,8 +214,6 @@ mod test {
         )
         .unwrap();
 
-        println!("{}", bitboard_to_str(calculate_west_ray_attack(d4)));
-
         assert_eq!(east_ray_d4, calculate_west_ray_attack(d4))
     }
 
@@ -179,8 +245,130 @@ mod test {
 
         let ray_attack = calculate_east_ray_attack(d4);
 
-        println!("ray attack \n{}", bitboard_to_str(ray_attack));
-
         assert_eq!(east_ray_d4, ray_attack)
+    }
+
+    #[test]
+    fn test_calculate_north_west_ray_attack() {
+        let correct_ray_attack = bitboard_from_str(
+            "....x...
+             ...x....
+             ........
+             ........
+             ........
+             ........
+             ........
+             ........",
+        )
+        .unwrap();
+
+        let d4 = bitboard_from_str(
+            "........
+             ........
+             ..x.....
+             ........
+             ........
+             ........
+             ........
+             ........",
+        )
+        .unwrap();
+
+        let calculated_ray_attack = calculate_north_west_ray_attack(d4);
+
+        assert_eq!(correct_ray_attack, calculated_ray_attack)
+    }
+
+    #[test]
+    fn test_calculate_south_east_ray_attack() {
+        let correct_ray_attack = bitboard_from_str(
+            "........
+             ........
+             ........
+             ........
+             ......x.
+             .....x..
+             ....x...
+             ...x....",
+        )
+        .unwrap();
+
+        let position = bitboard_from_str(
+            "........
+             ........
+             ........
+             .......x
+             ........
+             ........
+             ........
+             ........",
+        )
+        .unwrap();
+
+        let calculated_ray_attack = calculate_south_east_ray_attack(position);
+
+        assert_eq!(correct_ray_attack, calculated_ray_attack)
+    }
+
+    #[test]
+    fn test_calculate_north_east_ray_attack() {
+        let correct_ray_attack = bitboard_from_str(
+            "..x.....
+             ...x....
+             ....x...
+             .....x..
+             ........
+             ........
+             ........
+             ........",
+        )
+        .unwrap();
+
+        let position = bitboard_from_str(
+            "........
+             ........
+             ........
+             ........
+             ......x.
+             ........
+             ........
+             ........",
+        )
+        .unwrap();
+
+        let calculated_ray_attack = calculate_north_east_ray_attack(position);
+
+        assert_eq!(correct_ray_attack, calculated_ray_attack)
+    }
+
+    #[test]
+    fn test_calculate_south_west_ray_attack() {
+        let correct_ray_attack = bitboard_from_str(
+            "........
+             ........
+             ........
+             .x......
+             ..x.....
+             ...x....
+             ....x...
+             .....x..",
+        )
+        .unwrap();
+
+        let position = bitboard_from_str(
+            "........
+             ........
+             x.......
+             ........
+             ........
+             ........
+             ........
+             ........",
+        )
+        .unwrap();
+
+        let calculated_ray_attack = calculate_south_west_ray_attack(position);
+
+        assert_eq!(correct_ray_attack, calculated_ray_attack)
     }
 }
